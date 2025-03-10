4aab32ef8518b7d9fdda2e8b25d3fa4456447528
ilbuonmarcio <alessandro@goldmark.solutions>
2025-03-10 17:12:53 +0100

    Initial commit PoC

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,18 @@
-FROM python:3.13
+FROM python:3.13.2-bookworm
 
 RUN apt update && apt upgrade -y
+
+RUN apt install firefox-esr wget -y
+RUN wget https://github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.36.0-linux64.tar.gz && \
+    tar -xvzf geckodriver-v0.36.0-linux64.tar.gz && rm geckodriver-v0.36.0-linux64.tar.gz && ls -lah
+
 RUN mkdir /app
+RUN cp geckodriver /app/geckodriver
 WORKDIR /app
-COPY src/main.py /app/main.py
+
 COPY requirements.txt /app/requirements.txt
 RUN pip install -r /app/requirements.txt
+
+COPY src/main.py /app/main.py
+
+CMD ["python", "/app/main.py"]
--- src/main.py
+++ src/main.py
@@ -1,2 +1,60 @@
-if __name__ == "__main__":
-    print("Hello, World!")
+from loguru import logger
+from flask import Flask
+from flask import Response
+import base64
+from io import BytesIO
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--headless')
+service = Service(executable_path="/app/geckodriver")
+
+app = Flask(__name__)
+
+
+@app.route("/url-preview/<b64url>")
+def hello_world(b64url):
+    try:
+        url = base64.b64decode(b64url).decode('utf-8')
+        
+        # Getting the URL in the browser
+        driver = webdriver.Firefox(options=options, service=service)
+        driver.set_window_size(1200, 900)
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        driver.get(url)
+        
+        # Getting the actual screenshot
+        screenshot = driver.get_screenshot_as_png()
+        screenshot_buffer = BytesIO(screenshot)
+        
+        # Resizing the image
+        image = Image.open(screenshot_buffer)
+        resized_image = image.resize((540, 300), Image.LANCZOS)
+        
+        # Resized buffer
+        resized_buffer = BytesIO()
+        resized_image.save(resized_buffer, format="PNG")
+        
+        # Convert to base64
+        resized_bytes = resized_buffer.getvalue()
+        base64_string = base64.b64encode(resized_bytes).decode("utf-8")
+        
+        # Quitting the driver
+        driver.quit()
+        
+        return f'<img src="data:image/png;base64,{base64_string}"/>'
+    except Exception as e:
+        return Response(
+            "Error during URL request",
+            status=500
+        )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=42069)
